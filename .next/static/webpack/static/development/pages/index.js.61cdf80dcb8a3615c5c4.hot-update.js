87bf53e8347b3c27d95eee2f38133e14e27fb726
arpita tiwari <arpitatiwari309@gmail.com>
2020-03-26 21:16:21 +0530

    basic todo

---
+++ .next/static/webpack/static/development/pages/index.js.61cdf80dcb8a3615c5c4.hot-update.js
@@ -0,0 +1,64 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/Navbar.js":
+/*!*************************!*\
+  !*** ./pages/Navbar.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-jsx/style */ "./node_modules/styled-jsx/style.js");
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var reactstrap__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! reactstrap */ "./node_modules/reactstrap/es/index.js");
+var _this = undefined,
+    _jsxFileName = "C:\\Users\\at\\Desktop\\todo-next\\pages\\Navbar.js";
+
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+
+var Navbar = function Navbar() {
+  return __jsx("nav", {
+    className: "navbar navbar-expand navbar-dark bg-dark mb-4",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, __jsx("div", {
+    className: "jsx-3545143372" + " " + "container",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 9
+    }
+  }, __jsx("ul", {
+    className: "jsx-3545143372",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 10
+    }
+  }, "Todo app"), __jsx(styled_jsx_style__WEBPACK_IMPORTED_MODULE_0___default.a, {
+    id: "3545143372",
+    __self: _this
+  }, "ul.jsx-3545143372{background:#87CEEB;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkM6XFxVc2Vyc1xcYXRcXERlc2t0b3BcXHRvZG8tbmV4dFxccGFnZXNcXE5hdmJhci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFTaUIsQUFDcUIsbUJBQUMiLCJmaWxlIjoiQzpcXFVzZXJzXFxhdFxcRGVza3RvcFxcdG9kby1uZXh0XFxwYWdlc1xcTmF2YmFyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IExpbmsgZnJvbSAnbmV4dC9saW5rJztcclxuaW1wb3J0IHtOYXYsTmF2YmFyQnJhbmR9ZnJvbSAncmVhY3RzdHJhcCdcclxuY29uc3QgTmF2YmFyID0gKCkgPT4gKFxyXG4gICAgXHJcbiAgICAgICAgPG5hdiBjbGFzc05hbWU9XCJuYXZiYXIgbmF2YmFyLWV4cGFuZCBuYXZiYXItZGFyayBiZy1kYXJrIG1iLTRcIj5cclxuICAgICAgICA8ZGl2IGNsYXNzTmFtZT1cImNvbnRhaW5lclwiPlxyXG4gICAgICAgICA8dWw+VG9kbyBhcHA8L3VsPiAgIFxyXG4gICAgICAgICAgICBcclxuICAgICAgICAgICAgPHN0eWxlIGpzeD5cclxuICAgICAgICAgICAgICAgIHtgIHVse2JhY2tncm91bmQ6Izg3Q0VFQn0sXHJcbiAgICAgICAgICAgICAgICBgfVxyXG4gICAgICAgICAgICA8L3N0eWxlPlxyXG4gICAgICAgIDwvZGl2PlxyXG4gICAgICAgIDwvbmF2PlxyXG4gICAgXHJcbiBcclxuKTtcclxuXHJcbmV4cG9ydCBkZWZhdWx0IE5hdmJhcjsiXX0= */\n/*@ sourceURL=C:\\\\Users\\\\at\\\\Desktop\\\\todo-next\\\\pages\\\\Navbar.js */")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Navbar);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.61cdf80dcb8a3615c5c4.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+
+import { createGlobalStyle } from './global.style';
+
+
+const Index = () => (
+    <Home/>
+  )
+  
+  export default Index
